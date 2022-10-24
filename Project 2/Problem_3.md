f81eb6969786571ee873d76dc872502930af2ba4
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-10-24 00:48:27 -0400

    Finalizing Problem 3

--- Project 2/Problem_3.md
+++ Project 2/Problem_3.md
@@ -1,1 +1,21 @@
 # Problem 3
+
+This was a tricky problem to solve, and took me roughly five hours to land a solution that worked. I debated about what data structure to use, eventually deciding on an ordered Linked List with Frequency Nodes, or nodes that stored a letter and frequency. This made it easy to fill in the list with the Huffman Nodes, which stored a number and its two children nodes.
+
+Time complexity isn't great in the worst-case. Here it is for each function:
+
+- add(): where i is the index of the letter being added, O(i)
+
+- pop(): O(1)
+
+- convert_to_huffman(): where d is number of nodes in the linked list, O(d). However, n is rapidly shrinking, and typically wouldn't be larger than 26 (lowercase letters) + 26 (captial letters) = 52
+
+- create_translation() --> search(): O(1), as every action in the recursive function works in constant time 
+
+- encode(): where n is length of string to encode, O(n)
+
+- decode(): where n is length of string to decode, O(n)
+
+- huffman_encoding(): where n is number of characters in string to encode, O( n * O(i) + O(d) + O(n) ) ~ O( 2n * O(i) ) ~ O( n * O(i) )
+
+- huffman_decoding(): where n is length of string to decode, O( O(n) ) = O(n)
--- Project 2/Problem_3.py
+++ Project 2/Problem_3.py
@@ -97,7 +97,7 @@ class PriorityLinkedList:
                 huffnode.next = self.head
                 self.head = huffnode
             
-            print(self)
+            # print(self)
         
     def create_translation(self):
         self.search(self.head, "")
@@ -166,6 +166,9 @@ class PriorityLinkedList:
 
 # Returns both the encoded string and the tree used to encode it
 def huffman_encoding(data):
+    if data == None or data == "":
+        print("Can't encode an empty string!")
+        return None, PriorityLinkedList()
     priority = PriorityLinkedList()
     for l in data:
         priority.add(l)
@@ -181,9 +184,10 @@ def huffman_encoding(data):
     return priority.encode(data), priority
 
 def huffman_decoding(data,tree):
+    if data == None or data == "":
+        print("Can't decode an empty string!")
+        return None 
     return tree.decode(data)
-
-codes = {}
 
 a_great_sentence = "The bird is the word"
 
@@ -204,7 +208,30 @@ print ("The content of the encoded data is: {}\n".format(decoded_data)) # The bi
 # and two of them must include edge cases, such as null, empty or very large values
 
 # Test Case 1
+# First result when googling "long story"
+encoded, tree = huffman_encoding("""I HAVE SOME GOOD news. Next month, The Atlantic will once again send fiction home to our subscribers, in a special supplement that will accompany our May issue. On the newsstand, the supplement will be bound into the May magazine.
+
+The short story has been integral to The Atlantic since our first issue, in 1857, in which we published four stories, including “The Mourning Veil,” by Harriet Beecher Stowe. But as longtime, generously loyal readers know, for the past five years we have published fiction once a year in a special newsstand issue, rather than in any of our 10 subscriber issues. During what has been widely noted to be a “challenging” (read: harrowing) business environment for publishing, this has been a necessary compromise. But none of us has been particularly happy with it, and we have been searching for ways to once again place great fiction in front of all our readers.
+
+With our fiction issue last year, we began a partnership with Luminato, the Toronto Festival of Arts and Creativity, which shares our love of literature. Building on the success of that first outing, which included participation by some of our editors and authors in the festival, we have jointly decided this year to raise our ambition by creating the supplement, which will include, along with half a dozen short stories, a powerful essay on writing and loss by Joyce Carol Oates. We think—we hope!—we are seeing renewed interest in the short story. Last fall, we started a digital fiction series, publishing to the Amazon Kindle two short stories a month by authors like Christopher Buckley, Curtis Sittenfeld, and Paul Theroux. All told, The Atlantic is now publishing more fiction than it has since the mid-1970s.
+
+But I should admit that these fiction initiatives are experimental, provisional, part of our larger adventure through the seismically shifting landscape of letters. If our hardworking developers have pulled it off, by the time you read this note our Web site, TheAtlantic.com, will have relaunched with a new design and a superior system for finding the subjects you’re interested in and discovering new ideas you didn’t know you were looking for. We’ve also released two apps for the iPhone so far and are about to release a third, much improved, one.
+
+We are experimenting busily, in other words, with any new technology that emerges in this extraordinarily fertile era. If it looks like we’re making things up as we go along, the reason is that we are. To each “platform,” as they are now called in the trade, we are tailoring the Atlantic work that can fit it best—trying to help you make sense of the world, to keep you informed and entertained, through whatever medium you find most congenial. For our print magazine and our e-reader editions, we are continuing to devote months of reporting and writing to create pieces like Joshua Green’s profile in this issue of Treasury Secretary Timothy Geithner, and Robert D. Kaplan’s assessment of the war in Afghanistan. For the Web site each day, we produce dozens of posts analyzing breaking developments in politics, business, culture, technology, and other subjects, some of them longtime preoccupations of The Atlantic, others fairly new to all of us. As I write, on our site I can see posts popping up by James Fallows about Twitter, by Andrew Sullivan about the future of gays in the military, and by Ta-Nehisi Coates about the moral courage of Civil War General George Henry Thomas.
+
+What matters to us—in all the work that we do, on whatever platform may present itself—is the quality and consequence of an idea, and the clarity and power of its expression. We believe, and we believe that you believe, that of the many and proliferating means for communicating big ideas, one of the most effective, and therefore most enduring, is fiction.""")
+print(encoded)
+decoded = huffman_decoding(encoded, tree)
+print(decoded) # Should return the input, I don't want to paste all of that again.
 
 # Test Case 2
+encoded, tree = huffman_encoding("")
+print(encoded) # None
+decoded = huffman_decoding(encoded, tree)
+print(decoded) # None
 
 # Test Case 3
+encoded, tree = huffman_encoding("Udacity Mission Statement - Udacity is the trusted market leader in talent transformation. We change lives, businesses, and nations through digital upskilling, developing the edge you need to conquer what's next.")
+print(encoded)
+decoded = huffman_decoding(encoded, tree)
+print(decoded)
