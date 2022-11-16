c9d54cab7ff2ecbc3fbab3de7bebbffe407769df
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-11-15 19:02:22 -0500

    Problem 2

--- Project 3/Problem_2.py
+++ Project 3/Problem_2.py
@@ -3,8 +3,7 @@ def find_rot_index(arr):
     first = 0
     last = len(arr) - 1
     while len(arr[first:last+1]) > 2:
-        mid = ((last - first) // 2) + first
-        # print(first, mid, last, arr[first:last+1], len(arr[first:last+1]))
+        mid = (last + first) // 2
         
         if arr[mid] < arr[first]:
             # Middle is less than first, rotational index is in first half of array
@@ -15,9 +14,28 @@ def find_rot_index(arr):
         else:
             # Breakout to prevent endless loops in case provided array is not rotated
             return -1
-    # print(first, mid, last, arr[first:last+1], len(arr[first:last+1]))
     # During testing, it got down to the two elements where the rotation had happened, so the second one is the index wanted
     return last
+
+def binary_search(arr, first, last, target):
+    # Regular binary search    
+    while first <= last:
+        # Calculate midpoint
+        mid = (first + last) // 2
+        
+        if target == arr[mid]:
+            # If target is found
+            return mid
+        
+        elif target < arr[mid]:
+            # If target is smaller than midpoint, target is in first half of array
+            last = mid - 1
+        
+        else:
+            # If target is larger than midpoint, target is in second half of array
+            first = mid + 1
+    # Return -1 if target is not found
+    return -1
     
 
 
@@ -32,29 +50,15 @@ def rotated_array_search(input_list, number):
        int: Index or -1
     """
     rot_index = find_rot_index(input_list)
-    print("rot_index =", rot_index)
 
+    if input_list[len(input_list)-1] < number:
+        # if the last element is smaller, the number being searched for is 
+        # somewhere between 0 and rot_index
+        return binary_search(input_list, 0, rot_index, number)
+    else:
+        # If it isn't, then the number is between rot_index and len(input_list) - 1
+        return binary_search(input_list, rot_index, len(input_list) - 1, number)
 
-    first = rot_index
-    local_last = rot_index - 1
-    true_last = len(input_list) - 1
-    while len(input_list[first:last+1]) > 2:
-        mid = ((last - first) // 2) + first
-        # print(first, mid, last, input_list[first:last+1], len(input_list[first:last+1]))
-        
-        if input_list[mid] < input_list[first]:
-            # Middle is less than first, rotational index is in first half of array
-            last = mid
-        elif input_list[mid] > input_list[last]:
-            # Middle is greater than last, rotational index is in second half of array
-            first = mid
-    # print(first, mid, last, input_list[first:last+1], len(input_list[first:last+1]))
-    # During testing, it got down to the two elements where the rotation had happened, so the second one is the index wanted
-    return last
-
-
-
-    return index
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
@@ -71,12 +75,10 @@ def test_function(test_case):
         print("Fail")
 
 
-print("rot_index =", find_rot_index([4, 0, 1, 2, 3]))
-
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4, 5], 6])
 test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
 test_function([[6, 7, 8, 1, 2, 3, 4], 8])
 test_function([[6, 7, 8, 1, 2, 3, 4], 1])
 test_function([[6, 7, 8, 1, 2, 3, 4], 10])
-test_function([3, 4, 0, 1, 2], 4)
-test_function([3, 4, 0, 1, 2], 0)
+test_function([[3, 4, 0, 1, 2], 4])
+test_function([[3, 4, 0, 1, 2], 0])
