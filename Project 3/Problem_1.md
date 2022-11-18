0bd14f05b5d902e53a34195c99c478b638ebf30f
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-11-17 19:06:48 -0500

    Overhauling Problem 1

--- Project 3/Problem_1.md
+++ Project 3/Problem_1.md
@@ -1,7 +1,5 @@
 # Problem 1
 
-Seeing that 'expected time complexity is O(log(n))', I figured I should use a binary search algorithm. My process is to create an array of
-every value from 0 to the number, inclusive, and then utilize a binary search to find the floor of the square root of the number given.
+Seeing that 'expected time complexity is O(log(n))', I figured I should use a binary search algorithm. My process was originally to create an array of every number from 0 to the given number, but that was not very efficient. My second solution was to create the "frame" of an array, only actually using the midpoint and the left and right bounds of where the answer could be found. This new solution takes what actually was an O(n * log(n)) to the acceptable time complexity of O(log(n)), because each iteration of the binary search cuts the total amount of possible values in half.
 
-Given that each iteration of the binary search cuts the array in half, time complexity is O(log(n))
-Space complexity is O(n), since an array with a length equaling the number given is created.
+Space complexity is O(1)
--- Project 3/Problem_1.py
+++ Project 3/Problem_1.py
@@ -1,28 +1,27 @@
 # Template provided by Udacity
-def bin_search(num, arr):
-    # Repeat until only one value remains in the array
-    while len(arr) > 1:
-        # Calculate the middle_index
-        middle_index = (len(arr) - 1) // 2
-        
-        # Finding the middle element
-        middle = arr[middle_index]
-        # Calculating the square to avoid unneccessary calculations in the if-elif-else statement
-        squared = middle * middle
-        
-        if squared == num:
-            # The square root was found exactly
-            return middle
-        
-        elif squared > num:
-            # Square root is in the smaller half of the array
-            return bin_search(num, arr[0:middle_index])
-        
+def bin_search(num, lower, upper):
+    # Running a binary search without an array
+    
+    # Calculate midpoint between range
+    current = (upper + lower) // 2
+    # Calculating the square to avoid unneccessary calculations in the if-elif-else statement
+    squared = current**2
+    if squared == num:
+        # The square root was found exactly
+        return current
+    
+    elif squared < num:
+        # Square root is in the bigger half of the array
+        # Before running another search, make sure the next value up isn't over our goal (indicates the current value should be returned)
+        if (current + 1)**2 > num:
+            # If the next number up, squared, is bigger, then the floored square of the number should be the number we have now.
+            return current
         else:
-            # Square root is in the bigger half of the array
-            return bin_search(num, arr[middle_index+1:len(arr)])
-    # If this return statement is reached, it means there is only one value in the array, which is therefore the square root     
-    return arr[0]
+            return bin_search(num, current + 1, upper)
+    
+    else:
+        # Square root is in the smaller half of the array
+        return bin_search(num, lower, current)
 
 
 def sqrt(number):
@@ -34,13 +33,28 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
     """
-    # Create an array of [0, 1, 2...n-2, n-1, n]
-    allVals = list(range(number+1))
-    # Call to the recursive binary search
-    return bin_search(number, allVals)
+    # Check that value isn't negative
+    if number < 0:
+        return None
+    # Call to the recursive binary search, providing the middle value
+    return bin_search(number, 0, number)
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print ("Pass for 9" if  (3 == sqrt(9)) else "Fail")
+print ("Pass for 0" if  (0 == sqrt(0)) else "Fail")
+print ("Pass for 16" if  (4 == sqrt(16)) else "Fail")
+print ("Pass for 1" if  (1 == sqrt(1)) else "Fail")
+print ("Pass for 27" if  (5 == sqrt(27)) else "Fail")
+
+# My own test cases
+
+# Test every number from 0 to 100
+for n in range(101):
+    square_root = int(n**(1/2))
+    # print(square_root) # Testing
+    print ("Pass for " + str(n) if  (square_root == sqrt(n)) else "Fail for " + str(n))
+
+# Test very high number
+print ("Pass for 94673825" if  (9730 == sqrt(94673825)) else "Fail")
+
+# Test negative number
+print ("Pass for -1" if  (None == sqrt(-1)) else "Fail")
