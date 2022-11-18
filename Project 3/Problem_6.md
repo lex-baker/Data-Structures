3dff97546b7d5432687633c92711ad37aa03aa71
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-11-17 19:06:56 -0500

    Overhauling Problem 6

--- Project 3/Problem_6.md
+++ Project 3/Problem_6.md
@@ -1,36 +1,8 @@
 # Problem 6
 
-I may have misread the problem, but finding only the max and min of a function in a single traversal and O(n) time is pretty simple, provided I did it correctly and am not overlooking something major.
+To find the minimum and maximum of an unsorted array in a single traversal, I initialized two variables, 'max' and 'min', both set equal to the first value of the array. From there, I iterated over the array once, checking each value against 'max' first to see if it was the max, and if it wasn't, against 'min' to see if it was the min.
 
-For creating the O(n) sorting algorithm, I made three iterations and left them all well-commented.
 
----
-
-For min/max:
-
-Time complexity: O(n), and in a single traversal
+Time complexity: O(n), and in a single traversal of the array.
 
 Space complexity: O(1)
-
----
-
-For initial sort algorithm:
-
-Time complexity: O(2 * n) ~ O(n)
-
-Space complexity: O(n)
-
----
-
-For intermediate sort algorithm:
-
-Time complexity: O(4 * n) ~ O(n); Total n's come from: min/max, create array of None, fill array, create final array that leaves out None's.
-
-Space complexity: O(n + (range between values of array)) ~ O(n); Maybe more, not sure how to calculate the possibly much larger range between values
-
----
-
-For final sort algorithm:
-
-Time complexity: O(4 * n); Same as intermediate, because even though duplicated values are being interacted with, the total number of values doesn't exceed the original array's length
-Space complexity: O(n + (range between values of array)) ~ O(n); Same as intermediate, still not sure how to calculate.
--- Project 3/Problem_6.py
+++ Project 3/Problem_6.py
@@ -17,77 +17,8 @@ def get_min_max(ints):
 
     return (min, max)
 
-def initial_o_n_time_sort(arr):
-    # This sorting alogrithm is pretty specific (to integers within a range of 0 to (length of array - 1)) and not very space efficient
-    arr_length = len(arr) 
-    sorted_arr = []
-
-    # Create an array of None to the size of the unsorted array
-    for l in range(arr_length):
-        sorted_arr.append(None)
-    
-    # Move the values in the original array to their value's index place
-    for n in arr:
-        sorted_arr[n] = n
-
-    print(sorted_arr)
-    return sorted_arr
-
-def improved_o_n_time_sort(arr):
-    # This sorting alogrithm is slightly more versatile but even less space efficient
-    # Duplicate values will not be preserved
-    min, max = get_min_max(arr)
-    arr_length = (max - min) + 1
-    intermediate_arr = []
-
-    # Create an array of None to the size of the largest value in the array minus the smallest value
-    for l in range(arr_length):
-        intermediate_arr.append(None)
-    
-    # Move the values in the original array to their value's index place, which potentially leaves many None's behind
-    for n in arr:
-        intermediate_arr[n - min] = n
-
-    # Now, instead of removing values when they equal None, which has potential worst case time complexity of O(n), we make a third array
-    # Overall, that has a time complexity of O(n), and skips over value that equal None
-    sorted_arr = []
-
-    for n in intermediate_arr:
-        if n:
-            sorted_arr += [ n ]
 
 
-    print(sorted_arr)
-    return sorted_arr
-
-def final_o_n_time_sort(arr):
-    # This avoids detroying duplicate values, but has worst case time complexity of O(n) ????
-    min, max = get_min_max(arr)
-    arr_length = (max - min) + 1
-    intermediate_arr = []
-
-    for l in range(arr_length):
-        intermediate_arr.append(None)
-    
-    # In this algorithm, every duplicate value increments a counter (originally, it was an array of duplicated values, but that is neither
-    # space nor time efficient)
-    for n in arr:
-        if intermediate_arr[n - min] is None:
-            intermediate_arr[n - min] = [ n, 1 ]
-        else:
-            intermediate_arr[n - min][1] += 1
-
-    # Now, instead of removing values when they equal None, which has potential worst case time complexity of O(n), we make a third array
-    # Which overall has time complexity of O(n)
-    sorted_arr = []
-
-    for n in intermediate_arr:
-        if n:
-            for v in range(n[1]):
-                sorted_arr += [ n[0] ]
-
-    print(sorted_arr)
-    return sorted_arr
 
 ## Example Test Case of Ten Integers
 import random
@@ -95,10 +26,27 @@ import random
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
-# More testing
-test_array = [5, 2, 2, 9, 3, 6]
-
 print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
-print ("Pass" if ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == initial_o_n_time_sort(l)) else "Fail")
-print ("Pass" if ([2, 3, 5, 6, 9] == improved_o_n_time_sort(test_array)) else "Fail") # Ignores duplicates
-print ("Pass" if ([2, 2, 3, 5, 6, 9] == final_o_n_time_sort(test_array)) else "Fail") # Considers duplicates
+
+
+# My test cases
+
+for n in range(10):
+    arr = [i for i in range(random.randint(0, 48), random.randint(52, 100))]
+    random.shuffle(arr)
+    verified_answer = (min(arr), max(arr))
+    my_answer = get_min_max(arr)
+    print("Verified answer:", verified_answer, "and my answer:", my_answer)
+    print ("Pass" if (verified_answer == my_answer) else "Fail")
+
+
+print("\nEdge cases:\n")
+# Edge cases
+arr = [0]
+print ("Pass" if ((0, 0) == get_min_max(arr)) else "Fail")
+
+arr = [-2, 1]
+print ("Pass" if ((-2, 1) == get_min_max(arr)) else "Fail")
+
+arr = [1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 19, 0, 0]
+print ("Pass" if ((0, 19) == get_min_max(arr)) else "Fail")
