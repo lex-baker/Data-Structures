b637b4b0c84f8d32a02281ab44f88bdb3f97f7f6
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-12-08 13:54:45 -0500

    Update student_code.py

--- Project 4/student_code.py
+++ Project 4/student_code.py
@@ -1,31 +1,69 @@
 import math
 
-class map_node():
-    m = None
-    def __init__(self, node):
-        self.node = node
-        self.g = 0
-        self.h = 
+"""
+all_paths= [] # a bunch of whats below, ordered by f
+current path = 
+[
+    [1, 4, 2, 7],   # Path
+    42             # F total
 
-class map_path():
-    m = None
-    def __init__(self, start):
+]
+
+"""
 
 
 def shortest_path(m,start,goal):
-    print("shortest path called")
+
+    class map_node():
+        def __init__(self, number):
+            self.number = number
+            self.g = 0
+            self.h = math.sqrt( (m.intersections[goal][0] - m.intersections[number][0])**2 + (m.intersections[goal][1] - m.intersections[number][1])**2 )
+
+        def f(self):
+            return self.g + self.h
+
+    class map_path():
+        def __init__(self, next, g, path):
+            if path:
+                self.path = path
+                self.path.append(next)
+            else:
+                self.path = [next]
+            self.g = g or 0
+            self.h = find_cost(self.get_last, goal)
+            self.total_f = self.g + self.h
+
+        def add_node(self, number):
+            self.path.append(number)
+            return map_path(number, self.path[:])
+
+        def get_last(self):
+            return self.path[-1]
+
+        def f(self):
+            return self.total_f
+
+
     
-    map_node.m = m
+    print("shortest path called")
+  
+
+    first_path = map_path(start)
+    print(first_path.total_g)
+
 
     explored = []
-    frontier = [start]
-    unexplored = [] # Maybe not, idk yet
+    frontier = [map_node(start)]
+    all_paths = []
 
     while True:
-        node = frontier.pop(0)
-        explored.append(node)
+        path = frontier.pop(0)
+        explored.append(path.get_last())
 
         for n in m.roads[node]:
+            if n not in explored:
+
             
         
 
@@ -37,4 +75,5 @@ def shortest_path(m,start,goal):
     return
 
 def find_cost(map, point_a, point_b):
+    # Using this for point and goal gives h, but using this for point and next point gives g
     return math.sqrt(map.intersections[point_a]**2 + map.intersections[point_b]**2)
