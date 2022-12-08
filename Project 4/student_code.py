ec5201e19e2667c9f82ad3a15fda528a62c3bd19
Archos14 <93758252+Archos14@users.noreply.github.com>
2022-12-08 18:47:30 -0500

    Update student_code.py

--- Project 4/student_code.py
+++ Project 4/student_code.py
@@ -1,80 +1,52 @@
 import math
 
-"""
-all_paths= [] # a bunch of whats below, ordered by f
-current path = 
-[
-    [1, 4, 2, 7],   # Path
-    42             # F total
+# Create custom class for storing map paths and calculating f values
+class map_path():
+    def __init__(self, m, node, goal, g=0, path=[]):
+        # Set map and goal
+        self.m = m
+        self.goal = goal
+        # Set g to given g
+        self.g = g
+        # Figure out what to do with path
+        if path == []:
+            self.path = [node]
+        else:
+            self.path = path
+            
+            # Increment g if this isn't the first node being added
+            self.g += self.find_cost(self.get_last(), node)
+            
+            self.path.append(node)
+        
+        self.h = self.find_cost(self.get_last(), self.goal)
+        self.f = self.g + self.h
 
-]
+    def add_node(self, number):
+        return map_path(self.m, number, self.goal, self.g, self.path[:])
 
-"""
+    def get_last(self):
+        return self.path[-1]
+    
+    def find_cost(self, point_a, point_b):
+        # Using this for point and goal gives h, but using this for point and next point gives g
+        return math.sqrt( math.pow(self.m.intersections[point_b][0] - self.m.intersections[point_a][0], 2) + math.pow(self.m.intersections[point_b][1] - self.m.intersections[point_a][1], 2) )
+    
+    def __str__(self):
+        return str(self.path) + " and f = " + str(self.f)
+
 
 
 def shortest_path(m,start,goal):
+    # Initialize the path
+    best_path = map_path(m, start, goal)
     
-    class map_path():
-        def __init__(self, node, g=0, path=[]):
-            # Set g to given g
-            self.g = g
-            # Figure out what to do with path
-            if path == []:
-                self.path = [node]
-            else:
-                self.path = path
-                
-                # Increment g if this isn't the first node being added
-                self.g += self.find_cost(self.get_last(), node)
-                
-                self.path.append(node)
-                
-            
-            
-            self.h = self.find_cost(self.get_last(), goal)
-            self.f = self.g + self.h
-
-        def add_node(self, number):
-            return map_path(number, self.g, self.path[:])
-
-        def get_last(self):
-            return self.path[-1]
-        
-        def find_cost(self, point_a, point_b):
-            # Using this for point and goal gives h, but using this for point and next point gives g
-            return math.sqrt( math.pow(m.intersections[point_b][0] - m.intersections[point_a][0], 2) + math.pow(m.intersections[point_b][1] - m.intersections[point_a][1], 2) )
-        
-        def __str__(self):
-            return str(self.path) + " and f = " + str(self.f)
-
-
-    
-    print("shortest path called")
-  
-
-    best_path = map_path(start)
-
-    explored = [start]
-    frontier = []
-    all_paths = []
-
-    # fill frontier with values
-    for n in m.roads[start]:
-        new_path = best_path.add_node(n)
-        if len(frontier) > 0:
-            i = 0
-            while i < len(frontier) and new_path.f > frontier[i].f:
-                i += 1
-            frontier.insert(i, new_path)
-        else:
-                frontier.append(new_path)
-                
-    print("Current path's:", best_path.path, "f =", best_path.f)
-    myprint(frontier)
-    print("\n next iter \n")
+    # Initialize the explored and frontier lists
+    explored = []
+    frontier = [best_path]
     
     # Now iterate
-    while best_path.get_last() != goal or best_path.f > frontier[0].f:
+    while best_path.get_last() != goal or best_path.f >= frontier[0].f:
         best_path = frontier.pop(0)
         
         last_node = best_path.get_last()
@@ -90,31 +62,16 @@ def shortest_path(m,start,goal):
                     frontier.insert(i, new_path)
                 else:
                     frontier.append(new_path)
-        print("Current path's:", best_path.path, "f =", best_path.f)
-        myprint(frontier)
-        print("\n next iter \n")
-    
-    print("\n\n Finished \n\n")
+        #     print("Current path's:", best_path.path, "f =", best_path.f)
+        #     myprint(frontier)
+        #     print("\n next iter \n")
+        
+        # print("\n\n Finished \n\n")
     return best_path.path
 
-            
-# def find_cost(m, point_a, point_b):
-#     # Using this for point and goal gives h, but using this for point and next point gives g
-#     print(m.intersections[0])
-#     return math.sqrt( (m.intersections[point_b][0] - m.intersections[point_a][0])**2 + (m.intersections[point_b][1] - m.intersections[point_a][1])**2 )
-
-def myprint(arr):
-    output = "Frontier:\n"
-    for mp in arr:
-        output += str(mp)
-        output += "\n"
-    print(output)
-        
-
-    
-    
-    
-    
-    return
-
-
+# def myprint(arr):
+#     output = "Frontier:\n"
+#     for mp in arr:
+#         output += str(mp)
+#         output += "\n"
+#     print(output)
